a6f3a31953692aac0ac500c5162f076f51d334f3
WalleM <william.pscn@gmail.com>
2024-09-05 09:56:59 -0500

    Update function_app.py to register new service bus trigger

--- api/function_app.py
+++ api/function_app.py
@@ -1,7 +1,7 @@
 import azure.functions as func
-from additional_function_app import bp1
 from function_http_trigger import http_trigger_bp
+from function_svbus_trigger import svbus_trigger_bp
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
-app.register_functions(bp1)
 app.register_functions(http_trigger_bp) 
+app.register_functions(svbus_trigger_bp)
